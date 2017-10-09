e1899d3bc17f20365bac7ccf1a2f7bcd1102984e
Unknown <cameron.d.duncan@gmail.com>
2017-10-09 12:41:01 -0600

    Added PlayerMove.cs and started Ghost House

---
+++ Ghost House/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+	public float moveSpeed;
+	public float turnSpeed; 
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
