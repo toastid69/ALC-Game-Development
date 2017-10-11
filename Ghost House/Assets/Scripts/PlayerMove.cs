718e80921b093a03c2c6f2e636037e4f12c3c9a4
Unknown <cameron.d.duncan@gmail.com>
2017-10-11 13:09:12 -0600

    Finished PlayerMove.cs

--- Ghost House/Assets/Scripts/PlayerMove.cs
+++ Ghost House/Assets/Scripts/PlayerMove.cs
@@ -3,10 +3,16 @@ using System.Collections;
 
 public class PlayerMove : MonoBehaviour {
 	public float moveSpeed;
-	public float turnSpeed; 
+	public float turnSpeed;
+	public float jumpHeight; 
 	
-	// Update is called once per frame
 	void Update () {
-	
+	var j = Input.GetAxis("Jump")* Time.deltaTime * jumpHeight;
+	var y = Input.GetAxis("Horizontal")* Time.deltaTime * turnSpeed;
+	var z = Input.GetAxis("Vertical")* Time.deltaTime * moveSpeed;
+
+	transform.Rotate(0,y,0);
+	transform.Translate(0,0,z);
+	transform.Translate(0,j,0);
 	}
 }
