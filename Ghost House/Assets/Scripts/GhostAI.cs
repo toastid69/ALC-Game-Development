94a3ff312a432b5eff02a4f49c051d8147d3d159
Unknown <cameron.d.duncan@gmail.com>
2017-10-25 12:45:17 -0600

    Added Ghost and Flashlight

---
+++ Ghost House/Assets/Scripts/GhostAI.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostAI : MonoBehaviour 
+{
+	public float moveSpeed;
+
+	public Transform target;
+
+	public int damage;
+
+	void OnTriggerStay(Collider other)
+	{
+		if(other.gameObject.name == "Player")
+		{
+			transform.LookAt(target);
+			transform.Translate(Vector3.forward*moveSpeed*Time.deltaTime);
+		}
+
+	}
+
+	/*void onCollisionEnter(Collisio other)
+	{ 
+		var hit = other.gameObject;
+		var health = hit.GetComponent<playerHealth>();
+
+		if(health != null)
+		{
+			health.TakeDamage(damage);
+		}
+
+	} */
+}
